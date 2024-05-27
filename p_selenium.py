f5151428abd4168d667e1e644106354104fe4ff7
DokuroTW <100449940+DokuroTW@users.noreply.github.com>
2024-05-27 20:20:14 +0800

    Update p_selenium.py

--- p_selenium.py
+++ p_selenium.py
@@ -3,27 +3,24 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 
-# 使用 Chrome 瀏覽器
+# 使用 Chrome 
 service = Service(ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
 
-# 打開 Google 網頁
+# Google 網頁
 driver.get("https://www.google.com")
 
-# 找到搜索框
+# 找到搜索
 search_box = driver.find_element("name", "q")
 
-# 在搜索框中輸入 "Selenium"
+# 在搜索中輸入 "Selenium"
 search_box.send_keys("Selenium")
 
-# 模擬按下 Enter 鍵
+# 按下 Enter 鍵
 search_box.send_keys(Keys.ENTER)
 
-# 等待搜索結果加載
+# 等待搜索結果
 driver.implicitly_wait(5)
-
-# 打印頁面標題
-print(driver.title)
 
 # 關閉瀏覽器
 driver.quit()
